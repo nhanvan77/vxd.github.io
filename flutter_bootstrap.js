8d93a2e25c5df09bed636142c64d75dd36d4665d
nhanvan77 <36062280+nhanvan77@users.noreply.github.com>
2025-04-07 08:18:39 +0700

    sd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3187816951"
+    serviceWorkerVersion: "2427428610"
   }
 });
